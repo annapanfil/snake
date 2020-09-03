30d63027b5e75cfec07fc4de034efd572fdfb92a
Anna <134panka@gmail.com>
2020-09-03 13:14:26 +0200

    create window and game loop
    board is displayed in window

---
+++ main.py
@@ -0,0 +1,42 @@
+import pygame as pg
+from classes import *
+
+def main():
+    screen_size = 480
+
+    # INITIALIZE PYGAME AND CREATE THE WINDOW
+    pg.init()
+    screen = pg.display.set_mode((screen_size, screen_size))
+
+    # Title and icon
+    pg.display.set_caption("Snake game")
+    icon = pg.image.load("snake.png")
+    pg.display.set_icon(icon)
+
+    rectangle =  pg.Rect((100,50), (100,100))
+
+    board = Board(surface = screen)
+
+
+
+    # GAME LOOP
+    running = True
+    while running:
+        # EVENTS HANDLING
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                running = False
+
+        board.display()
+        # screen.fill((0, 0, 255)) # fill the screen with colour
+        # screen.blit(icon, (100,100)) # show the picture
+        pg.display.update()
+
+
+
+    # clock = pg.time.Clock()
+    # surface = pg.Surface((screen_size,screen_size))
+    # surface = surface.convert()
+
+if __name__ == '__main__':
+    main()
