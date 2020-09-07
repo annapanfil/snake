a0accaeba0b707e594a4fa4eb9cd38013cae385b
Anna <134panka@gmail.com>
2020-09-07 20:32:17 +0200

    add pl translation pack

---
+++ language.py
@@ -0,0 +1,71 @@
+en={
+    #MENU
+    'menu_window': 'Snake – MENU'
+    'welcome': '\nWelcome to snake game!\n',
+    'start': 'Start game',
+    'settings': 'Settings',
+    'info': 'Info',
+    'exit': "Exit",
+
+    #SETTINGS
+    'settings_window': 'Snake – SETTINGS',
+    'board_size': 'Board size:\t',
+    'food_quantity': 'Quantity of food:\t',
+    'speed': "Snake speed:\t",
+    'speeds': ["slow", "normal", "fast", "lightning fast"]
+    'speed_increase': 'Increasing speed',
+    'wall_die': "Die from walls",
+    'show_score': "Show score",
+    'save': 'Save',
+
+    #INFO
+    'info_window':  'Snake – INFO',
+    'h1_controls': "CONTROLS",
+    'controls': 'Move snake:\tarrows or ADWS\nPause:\t\tP or space',
+    'h1_rules': "RULES",
+    'rules': "Eat food to increase length (and the score).\nDo not eat your tail.\nDo not hit the wall (or teleport after touching the wall – choose in settings).",
+    'credits': 'Code available on GitHub: github.com/panka134\n©Anna Panfil 2020',
+    'back' : "Back",
+
+    #EXIT
+    'exit_window': "Snake – GAME OVER",
+    'game_over': "\nGAME OVER",
+    'score': 'Your score: {score}\n',
+    'play_again': "Play again"
+    }
+
+pl={
+    #MENU
+    'menu_window': 'Snake – MENU'
+    'welcome': '\nWitaj w grze Snake!\n',
+    'start': 'Rozpocznij grę',
+    'settings': 'Ustawienia',
+    'info': 'Informacje',
+    'exit': "Wyjście",
+
+    #SETTINGS
+    'settings_window': 'Snake – USTAWIENIA',
+    'board_size': 'Wielkość planszy:\t',
+    'food_quantity': 'Ilość jedzenia:\t',
+    'speed': "Prędkość węża:\t",
+    'speeds': ["wolna", "normalna", "szybka", "błyskawiczna"]
+    'speed_increase': 'Stopniowo przyspieszanie',
+    'wall_die': "Śmierć po uderzeniu w ścianę",
+    'show_score': "Pokazywanie wyniku",
+    'save': "Zapisz",
+
+    #INFO
+    'info_window':  'Snake – INFORMACJE',
+    'h1_controls': "STEROWANIE",
+    'controls': 'Poruszanie się:\tstrzałki lub ADWS\nPauza:\t\tP lub spacja',
+    'h1_rules': "ZASADY",
+    'rules': "Jedz, aby zwiększyć długość węża (i wynik).\nNie jedz wężowego ogona.\nNie uderzaj w ściany (albo przenieś się na drugą stronę, uderzając w ścianę – wybierz w ustawieniach).",
+    'credits': 'Kod dostępny na GitHubie: github.com/panka134\n©Anna Panfil 2020',
+    'back' : "Powrót",
+
+    #EXIT
+    'exit_window': "Snake – KONIEC GRY",
+    'game_over': "\nKONIEC GRY",
+    'score': 'Twój wynik: {score}\n',
+    'play_again': "Zagraj ponownie"
+    }
